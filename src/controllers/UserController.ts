6c3e419bb940411a55a9c75fb338501d5bad88a6
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-07 19:46:01 -0300

    feat: Criando o UserController e o createUser

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,31 @@
 import { Request, Response } from 'express'
+import { prisma } from '../database'
 
 export default {
     async createUser(request: Request, response: Response) {
         try {
             const { name, email } = request.body
-            const userExists = await 
+            const userExists = await prisma.user.findUnique({ where: {email} })
+
+            if(userExists){
+                return response.json({
+                    error: true,
+                    message: 'Error: User already exists!'
+                })
+            }
+
+            const user = await prisma.user.create({
+                data: {
+                    name,
+                    email
+                }
+            })
+
+            return response.json({
+                error: false,
+                message: 'Success: User created successfully!',
+                user
+            })
 
         } catch (error) {
             return response.json({message: error.message})
