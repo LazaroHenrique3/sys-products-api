3a4974e79efbb44c8799f0c8f744a7f2eac55299
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2023-11-08 10:58:48 -0300

    feat: Criando o PostController e a função createPost

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config'
 import Express from 'express'
+import UserController from './controllers/UserController'
+import PostController from './controllers/PostController'
 
 const app = Express()
 app.use(Express.json())
 
 const PORT = process.env.PORT ? process.env.PORT : 8000
+
+app.post('/createUser', UserController.createUser)
+app.post('/createPost', PostController.createPost)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
